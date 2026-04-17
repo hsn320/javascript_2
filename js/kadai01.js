ca5555b3003e570a28b9fa995f7dd7289e4aa00b
hsn320 <2250025@ecc.ac.jp>
2026-04-17 09:16:58 +0900

    kadai01途中

---
+++ js/kadai01.js
@@ -0,0 +1,9 @@
+const setBtn = document.querySelectorAll("set-btn");
+const clearBtn = document.querySelector("clear-btn");
+const imgArea = document.querySelector("#img-field img");
+
+console.log(setBtn,clearBtn,imgArea);
+
+setBtn.onclick = () => {
+    imgArea = button.setAttribute("data-name");
+}
