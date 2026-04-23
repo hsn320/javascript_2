3965dd011c75aacae5873404e1f32d2739c9545f
hsn320 <2250025@ecc.ac.jp>
2026-04-23 10:30:37 +0900

    kadai01完成

--- js/kadai01.js
+++ js/kadai01.js
@@ -1,9 +1,32 @@
-const setBtn = document.querySelectorAll("set-btn");
-const clearBtn = document.querySelector("clear-btn");
+const setBtn = document.querySelectorAll(".set-btn");
+const clearBtn = document.querySelector(".clear-btn");
 const imgArea = document.querySelector("#img-field img");
 
-console.log(setBtn,clearBtn,imgArea);
+// 画像フォルダの名前
+const imageDir = "img";
 
-setBtn.onclick = () => {
-    imgArea = button.setAttribute("data-name");
+// デフォルトの画像ファイル名
+const defaultImagePath = imgArea.getAttribute("src");
+
+// console.log(setBtn,clearBtn,imgArea);
+
+for( let i = 0; i < setBtn.length; i++){
+    setBtn[ i ].addEventListener("click", (event) => {
+        const button = event.currentTarget;
+        
+        // btnタグに設定されているData属性を使って、画像パスを作る
+        // 画像パスを作る
+        const filePath = `${imageDir}/${button.dataset.name}.${button.dataset.ex}`;
+
+        // imgタグのsrc属性を変更する
+        // imgArea.src = filePath;
+        imgArea.setAttribute("src", filePath);
+    });
 }
+
+// .clear-btn click event
+clearBtn.addEventListener("click",(event) => {
+
+    // imgタグのsrc属性を変更する
+    imgArea.setAttribute("src", defaultImagePath);
+});
