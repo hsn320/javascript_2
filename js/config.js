9f18bff0bc0bf348359e41138415df2adfe0269f
hsn320 <2250025@ecc.ac.jp>
2026-07-09 10:44:46 +0900

    sample05

--- js/config.js
+++ js/config.js
@@ -1,1 +1,2 @@
-const SAMPLE08_URL = "http://click.ecc.ac.jp/ecc/huemori/php1/sample08-1.php";
+export const SAMPLE08_URL = "http://click.ecc.ac.jp/ecc/huemori/php1/sample08-1.php";
+export const API_PHP1_PRODUCTS = "http://10.202.10.3/ecc/huemori/php1/api/products/"
--- js/sample05-1.js
+++ js/sample05-1.js
@@ -1,4 +1,6 @@
 
+import { SAMPLE08_URL, API_PHP1_PRODUCTS, } from "./config.js";
+
 // 非同期通信(ajax)
 // thenで処理を順番通りに繋ぐ
 // const fetchSweetsData = fetch("https://click.ecc.ac.jp/ecc/huemori/php1/sample08-1.php")
@@ -13,3 +15,22 @@ const fetchSweetsData = await fetch(SAMPLE08_URL)
 
 console.log(fetchSweetsData);
 
+// GETでデータ送信
+const getParams = "name=シェイク";
+const fetchPHP1ProductsData = await fetch(`${API_PHP1_PRODUCTS}?${getParams}`)
+    .then(res => res.json());
+
+console.log(fetchPHP1ProductsData);
+
+// .products へ liを追加する liの表示は商品名
+const productArea = document.querySelector(".product");
+let productListItemArea = "";
+
+fetchPHP1ProductsData.forEach((product) => {
+    productListItemArea += `<li>${product.name}</li>`;
+});
+
+productArea.insertAdjacentHTML(
+    "beforeend",
+    productListItemArea
+);
