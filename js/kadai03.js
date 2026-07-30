abe4fadb2d99e04a1033d6c8ee76d8e77e7155fb
hsn320 <2250025@ecc.ac.jp>
2026-07-30 10:45:52 +0900

    追加：kadai05,03

--- js/kadai03.js
+++ js/kadai03.js
@@ -2,18 +2,41 @@
 // [type=submit]
 const entryBtn = document.querySelector("[type=submit]");
 // [name=name]
-const productNameField = document.querySelector("[name=name");
+const productNameField = document.querySelector("[name=name]");
+const authorNameField = document.querySelector("[name=author_name]");
+const productUrlField = document.querySelector("[name=product_url]");
+const videoUrlField = document.querySelector("[name=video_url]");
+const productCategoryField = document.querySelector("[name=product_category]");
+const descriptionField = document.querySelector("[name=description]");
 
 // ページを訪問時
 
 // ??(NULL合体演算子) ?? の右辺が null の場合は、?? の右辺のデータを保存する
 let productName = sessionStorage.getItem( "kadai03_product_name" ) ?? "";
+let authorName = sessionStorage.getItem("kadai03_author_name") ?? "";
+let productUrl = sessionStorage.getItem("kadai03_product_url") ?? "";
+let videoUrl = sessionStorage.getItem("kadai03_video_url") ?? "";
+let productCategory = sessionStorage.getItem("kadai03_product_category") ?? "";
+let description = sessionStorage.getItem("kadai03_description") ?? "";
+
+productNameField.value = productName;
+authorNameField.value = authorName;
+productUrlField.value = productUrl;
+videoUrlField.value = videoUrl;
+productCategoryField.value = productCategory;
+descriptionField.value = description;
 
 // [type=submit] click event
 entryBtn.addEventListener("click",(e) =>{
     // formのsubmit送信を停止
     e.preventDefault();
+
     if(productNameField.value !== ""){
-        sessionStorage.setItem( "kadai03_product_name", productNameField.value );
+        sessionStorage.setItem( "kadai03_product_name", productNameField.value);
+        sessionStorage.setItem("kadai03_author_name", authorNameField.value);
+        sessionStorage.setItem("kadai03_product_url", productUrlField.value);
+        sessionStorage.setItem("kadai03_video_url", videoUrlField.value);
+        sessionStorage.setItem("kadai03_product_category", productCategoryField.value);
+        sessionStorage.setItem("kadai03_description", descriptionField.value);
     }
 });
--- js/kadai05.js
+++ js/kadai05.js
@@ -2,9 +2,7 @@ import { API_KADAI05 } from "./config.js";
 
 const fetchData = await fetch(API_KADAI05, {
     method: "GET",
-    headers: {},
-    body: {},
-    catch: "no-store",
+    cache: "no-store",
 });
 const APIData = await fetchData.then
 
